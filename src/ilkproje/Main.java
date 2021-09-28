3c5ba8f8dc9e52d7eb1f852f1af801b8ec3ff2cf
emir14akbasoglu <eakba7197@gmail.com>
2021-09-28 16:01:07 +0300

    aesa

--- src/ilkproje/Main.java
+++ src/ilkproje/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Merhaba Java");
-System.out.print("murat");
-System.out.print(" ");
-System.out.println("");//println bayram yazdýktan sonra imleci alt satýra indirir
-System.out.println("emir"+" "+"akbasoğlu");
-System.out.println("Yaþ :"+15);
+		System.out.println("Merhaba Java");
+		System.out.print("murat");
+		System.out.print(" ");
+		System.out.println("");// println bayram yazdýktan sonra imleci alt satýra indirir
+		System.out.println("emir" + " " + "akbasoğlu");
+		System.out.println("Yaþ :" + 15);
 	}
 
 }
