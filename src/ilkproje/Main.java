8a4c60a26aaacdc13f990f0bd6eb816549ee0426
emir14akbasoglu <eakba7197@gmail.com>
2021-09-28 15:59:42 +0300

    hdjhfljdl

---
+++ src/ilkproje/Main.java
@@ -0,0 +1,15 @@
+package ilkproje;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Merhaba Java");
+System.out.print("murat");
+System.out.print(" ");
+System.out.println("");//println bayram yazdýktan sonra imleci alt satýra indirir
+System.out.println("emir"+" "+"akbasoğlu");
+System.out.println("Yaþ :"+15);
+	}
+
+}
