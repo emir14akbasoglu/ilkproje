b38f9acd6a2187f096bf201430573d7431ebba5e
emir14akbasoglu <eakba7197@gmail.com>
2021-09-28 14:01:37 +0300

    hggjgjgh

---
+++ src/ilkproje/Main.java
@@ -0,0 +1,19 @@
+package ilkproje;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//sysout ctrl sapace
+		System.out.println("MERHABA JAVA");
+		System.out.print("emir");
+		System.out.print(" ");
+		System.out.println("akbasoglu");
+		System.out.println("Murat"+" "+"emir");
+		System.out.println("Yaþ :  "+31);
+		
+	
+
+	}
+
+}
